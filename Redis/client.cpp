9a314b55c8e92a94d93b96fef65aaa4933c9c540
George Yu <george@think.digital>
2024-08-12 22:12:00 +0930

    Update server to process multiple requests from client

--- Redis/client.cpp
+++ Redis/client.cpp
@@ -7,13 +7,99 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <cassert>
+
 
 using namespace std;
+
+const size_t k_max_msg = 4096;
 
 void die(const string &msg) {
     cout << msg << endl;
     exit(EXIT_FAILURE);
 }
+
+void msg(const string &msg) {
+    cout << msg << endl;
+}
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv; 
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0){
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+// client makes requests and receive responses
+static int32_t query(int fd, const char* text) {
+    uint32_t len = (uint32_t) strlen(text);
+
+    // check if message over limit
+    if (len > k_max_msg) {
+        return -1;
+    }
+
+    // copy msg to write buffer and send request
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+
+    if (int32_t err = write_all(fd, wbuf, 4+len)) {
+        return err;
+    }
+
+    // receive response from server and process
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+    
+    // check if response too long
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // process response body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    rbuf[4 + len] = '\0';
+    cout << "server says: " << &rbuf[4] << endl;
+    return 0;
+}
+
+
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -32,17 +118,21 @@ int main() {
         die("connect");
     }
 
-    string msg = "hello";
-    write(fd, msg.c_str(), msg.length());
-
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        die("read");
+    int32_t err = query(fd, "hello1");
+    if (err) {
+        goto L_DONE;   
     }
-    printf("server says: %s\n", rbuf);
-    close(fd);
+    err = query(fd, "hello2");
+    if (err) {
+        goto L_DONE;   
+    }
+    err = query(fd, "hello3");
+    if (err) {
+        goto L_DONE;   
+    }
 
+L_DONE:
+    close(fd);
     return 0;
 }
 
--- Redis/server.cpp
+++ Redis/server.cpp
@@ -7,8 +7,12 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <cassert>
+
 
 using namespace std;
+
+const size_t k_max_msg = 4096;
 
 void die(const string &msg) {
     cout << msg << endl;
@@ -19,18 +23,70 @@ void msg(const string &msg) {
     cout << msg << endl;
 }
 
-// read and write file
-void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        msg("read() error");
-        return;
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv; 
+        buf += rv;
     }
-    cout << "client says: " << rbuf << endl;
+    return 0;
+}
 
-    string wbuf = "world";
-    write(connfd, wbuf.c_str(), wbuf.length());
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0){
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t) strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main() {
@@ -74,7 +130,15 @@ int main() {
             continue;
         }
 
-        do_something(connfd);
+        // serve one client connection at once
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
+
+        // do_something(connfd);
         close(connfd);
     }
 
